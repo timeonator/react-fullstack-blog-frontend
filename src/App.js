b56ddf32f655ee493443f25b99d7bb66afb0579d
timeonator <timeonator@gmail.com>
2021-03-18 21:20:41 -0700

    added nav to App

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {
     BrowserRouter as Router,
     Route
 } from 'react-router-dom';
+import NavBar from './navbar';
 import AboutPage from './pages/about-page';
 import ArticlePage from './pages/article-page';
 import ArticleList from './pages/article-list';
@@ -11,14 +12,15 @@ import HomePage from './pages/home-page'
 function App() {
   return (
     <Router>
+
     <div className="App">
-        <div id="page-body">
+        <NavBar />
+            <div id="page-body">
             <Route path="/" component={HomePage} exact />
             <Route path="/article" component={ArticlePage} />
             <Route path="/articlelist" component={ArticleList} />
             <Route path="/about" component={AboutPage} />            
         </div>
-
     </div>        
     </Router>
 
