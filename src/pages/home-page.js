00cbb8a32cb18d11e4f28f8a867aee2d7708af0d
timeonator <timeonator@gmail.com>
2021-03-18 19:54:10 -0700

    initial checkin

---
+++ src/pages/home-page.js
@@ -0,0 +1,27 @@
+import React from "react"
+import reactDom from "react-dom"
+
+const HomePage = () => {
+    return(
+    <>
+        <div className="home-page">
+            <h3>Welcome to my blog</h3>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Faucibus interdum posuere lorem ipsum dolor sit amet consectetur. Viverra nibh cras pulvinar mattis nunc sed blandit libero volutpat. Aliquet lectus proin nibh nisl condimentum id venenatis a condimentum. Netus et malesuada fames ac turpis. Suspendisse ultrices gravida dictum fusce ut placerat. Duis at consectetur lorem donec massa sapien faucibus et. Tellus at urna condimentum mattis pellentesque. Sem fringilla ut morbi tincidunt augue interdum velit. Felis imperdiet proin fermentum leo vel orci porta non pulvinar. Aliquet lectus proin nibh nisl condimentum id venenatis a condimentum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames. Sed vulputate mi sit amet.
+        </p>
+        <p>
+            Egestas integer eget aliquet nibh praesent. In metus vulputate eu scelerisque felis imperdiet proin fermentum. Risus sed vulputate odio ut enim blandit volutpat maecenas volutpat. Dolor magna eget est lorem ipsum dolor sit amet. Pharetra diam sit amet nisl suscipit adipiscing. Sodales ut etiam sit amet nisl. Cras adipiscing enim eu turpis egestas pretium aenean. Mi in nulla posuere sollicitudin aliquam ultrices sagittis orci. Pretium viverra suspendisse potenti nullam ac. Amet est placerat in egestas erat imperdiet sed euismod nisi. Nibh tortor id aliquet lectus. Diam quis enim lobortis scelerisque fermentum dui faucibus in ornare. A diam maecenas sed enim. Interdum consectetur libero id faucibus nisl tincidunt eget. Feugiat pretium nibh ipsum consequat nisl vel pretium lectus. Et netus et malesuada fames. Quam nulla porttitor massa id neque aliquam vestibulum morbi.
+        </p>
+        <p>
+            Adipiscing bibendum est ultricies integer quis. Vitae auctor eu augue ut. Augue neque gravida in fermentum et sollicitudin ac orci. Tortor dignissim convallis aenean et tortor at risus. Lacus vestibulum sed arcu non odio. Proin fermentum leo vel orci. Lorem sed risus ultricies tristique nulla aliquet enim tortor. Mi tempus imperdiet nulla malesuada pellentesque elit. Mauris in aliquam sem fringilla ut morbi tincidunt augue interdum. Pulvinar pellentesque habitant morbi tristique senectus. Magna ac placerat vestibulum lectus mauris ultrices. Facilisi cras fermentum odio eu feugiat. Id neque aliquam vestibulum morbi blandit cursus risus at ultrices. Nunc congue nisi vitae suscipit tellus.
+        </p>
+        <p>
+            Molestie a iaculis at erat pellentesque adipiscing commodo elit. Diam vel quam elementum pulvinar etiam. Consequat nisl vel pretium lectus quam id. Tincidunt eget nullam non nisi est. Cras semper auctor neque vitae tempus quam. Eget aliquet nibh praesent tristique. Interdum varius sit amet mattis vulputate enim nulla. Praesent elementum facilisis leo vel fringilla est ullamcorper eget nulla. Tellus orci ac auctor augue mauris. Sit amet risus nullam eget. Vitae congue mauris rhoncus aenean. Augue mauris augue neque gravida in fermentum et. Arcu cursus euismod quis viverra nibh.
+        </p>
+        <p>
+            Porttitor rhoncus dolor purus non enim praesent elementum facilisis. Et odio pellentesque diam volutpat commodo sed egestas egestas fringilla. Eget magna fermentum iaculis eu non diam phasellus vestibulum lorem. Nisi scelerisque eu ultrices vitae auctor eu. Mauris pharetra et ultrices neque. Arcu odio ut sem nulla pharetra diam. Risus feugiat in ante metus dictum. Neque sodales ut etiam sit amet. Ultricies mi eget mauris pharetra et ultrices neque ornare. Ut sem viverra aliquet eget sit amet tellus cras adipiscing. Mattis pellentesque id nibh tortor id aliquet lectus.
+        </p>
+    </div>
+    </>
+    );}
+export default HomePage;
