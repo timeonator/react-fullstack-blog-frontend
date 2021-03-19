1bcf508e8c9d97120f84ea9ab2ccefb4bf1f3f76
timeonator <timeonator@gmail.com>
2021-03-18 20:34:00 -0700

    routing and pages initial checkin

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import './App.css';
+import {
+    BrowserRouter as Router,
+    Route
+} from 'react-router-dom';
+import AboutPage from './pages/about-page';
+import ArticlePage from './pages/article-page';
+import ArticleList from './pages/article-list';
 import HomePage from './pages/home-page'
 
 function App() {
   return (
+    <Router>
     <div className="App">
-        <HomePage />
-    </div>
+        <div id="page-body">
+            <Route path="/" component={HomePage} exact />
+            <Route path="/article" component={ArticlePage} />
+            <Route path="/articlelist" component={ArticleList} />
+            <Route path="/about" component={AboutPage} />            
+        </div>
+
+    </div>        
+    </Router>
+
   );
 }
 
