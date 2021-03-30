84460a21cc5285f92ca21c442b907e68cf137f55
timeonator <timeonator@gmail.com>
2021-03-29 23:59:26 -0700

    checkpoint - setup fetch for articleInfo

--- src/pages/article-page.js
+++ src/pages/article-page.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import ArticleList from '../components/article-list'
 import PageNotFound from './page-not-found'
 import articleContent from './article-content'
@@ -7,11 +7,22 @@ const ArticlePage = ({match}) => {
     const name = match.params.name
     const article = articleContent.find(article => article.name === name);
     const otherArticles = articleContent.filter(article => article.name !== name)
+    const [articleInfo, setArticleInfo] = useState({upvotes: 0, comments : []});
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await fetch(`http://localhost:8000/api/articles/${name}`);
+            const body = await result.json();
+            setArticleInfo(body);
+        }
+        fetchData();
+    },[name]);
+
     if(!article) return <PageNotFound /> 
     
     return(
     <>
-        <h3>{article.name} : {article.title}</h3>
+        <h3>{article.title}</h3>
+        <p>This article has been upvoted {articleInfo.upvotes} times</p>
         {/* article.content is a array of paragraphs */}
         <div>
             {article.content.map((paragraph, key) => {
