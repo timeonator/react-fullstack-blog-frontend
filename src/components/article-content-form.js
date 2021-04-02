769a5ce8046432b09e4b247da8893cd76c47fcad
timeonator <timeonator@gmail.com>
2021-04-01 21:40:39 -0700

    Early musing on content form. Not integrated.

---
+++ src/components/article-content-form.js
@@ -0,0 +1,20 @@
+import React, {useState} from 'react'
+
+
+const ArticleContentForm = () => {
+    [username, setUserName] = useState("");
+    [articleName, setArticleName]= useState("");
+    [articleTitle, setArticleTitle]= useState("");
+    [articleContent, setArticleContent] = useState("");
+    return(
+        <div className='article-content-form'>
+            <input type="text" onChange={setUserName} value= {username} />
+            <input type="text" onChange={setArticleName} value= {articleName} />
+            <input type="text" onChange={setArticleTitle} value= {articleTitle} />
+            <input type="textarea" onChange={setArticleContent} value={articleContent}
+        </div>  
+    );
+
+    
+}
+export default ArticleContentForm;
